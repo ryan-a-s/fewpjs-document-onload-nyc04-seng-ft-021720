72a844a4881364e7ed1d5a85d266e21e50b71639
Ryan Seit <ryan@ryanseit.com>
2020-04-01 08:36:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+function changeText() {
+    let textArea = document.getElementById("text");
+    textArea.textContent = "This is really cool!";
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    changeText();
+}
+);
